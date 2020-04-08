a9cc96a31defe0ed6c8b6917445449b2ef24db94
tostitos <corellanav@buk.cl>
2020-04-08 17:25:10 -0400

    feature: devolver texto linea a linea con su respectivo salto de línea.

--- lib/plaintext/resolver.rb
+++ lib/plaintext/resolver.rb
@@ -38,8 +38,6 @@ module Plaintext
       if handler = find_handler and
           text = handler.text(@file, max_size: max_plaintext_bytes)
 
-        text.gsub!(/\s+/m, ' ')
-        text.strip!
         text.mb_chars.compose.limit(max_plaintext_bytes).to_s
       end
     end
